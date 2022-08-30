9c3ce75361e2e3dc22c1b1a40b0fdde4bf38faee
Andrea <56299876+Andrea-Paperini@users.noreply.github.com>
2022-08-30 02:22:39 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(RutApp());
+
+class RutApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Andrea',
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Gelateria da Andrea'),),
+        body: Builder(builder: (context) {
+          return SingleChildScrollView(
+          child: Column(children: [
+            Container(height: 20,),
+    Text('Il gelato migliore del mondo',
+    style: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),),
+            Container(height: 20,),
+    Image.network('https://mammaioparto.com/wp-content/uploads/2020/04/mettere-il-soggetto-al-centro.jpg'),
+            Container(height: 20,),
+    ElevatedButton(
+    onPressed: () {
+    SnackBar snackBar = SnackBar(
+    content: Text('la mail di Andrea è andrea@gmail.com'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    },
+    child: Text('Informazioni',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+    )
+    )
+    ],
+    ));
+    })),
+    );
+  }
+}
